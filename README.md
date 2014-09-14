a010221ce46800745ca540251269151d1a1879c3
SlightlyCyborg <taggartmachines@gmail.com>
2014-09-14 21:33:03 +0000

    Updated mission

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 GlobalHypothesis
 ================
 
-Mission: This is an app that will allow members to test hypotheses __collaboritivly__ online.
+Mission: This is an app that will allow members to  __collaboritivly__  build __general A.I. to test hypotheses__ online.
 
 How to contribute: Sign up or sign into github, fork the code, edit this readme by adding your name as a contributer, and push it up. Congratulations, you are contributing.  
+
+Reccomended Reading: 
+Steve Jobs by Walter Isaacson
+
+
 
 Contributors:
 SlightlyCyborg 4:46 EST (Knoxville, TN) Sep 14, 2014 
---
+++ StartHereSpirit.py
@@ -0,0 +1,12 @@
+print ("Hello World I am Cyborg, your python programmed guide to this repo . Welcome to Reddit's first collaborative coding project.")
+
+
+#feel free to rename this variable
+#
+first_level_info =  [{"title":"0) Mission", "message":"To build a collaboritave platform to test hypothesis."}, {"title":"1)Challenge", "message":"The challenge is not building the platform but rather testing the levels of cooperation that Reddit Has"}]
+for title in first_level_info:
+    print (title)
+choice = input("Please select by typing 0-9 and pressing ENTER")
+print((first_level_info[choice])["title"])
+
+                  
